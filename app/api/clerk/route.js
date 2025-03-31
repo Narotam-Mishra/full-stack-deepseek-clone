b80715bdd892cf284fbe50f2aa556debd6addd23
Narotam-Mishra <narotam21jan@gmail.com>
2025-03-31 23:14:57 +0530

    deepseek-clone-backend_added_webhook_for_clerk_p1_3.3

---
+++ app/api/clerk/route.js
@@ -0,0 +1,29 @@
+
+import { Webhook } from "svix";
+import connectDB from '@/config/db';
+import User from "@/models/User";
+import { headers } from "next/headers";
+
+export async function POST(req) {
+    const wh = new Webhook(process.env.SIGNIN_SECRET);
+    const headerPayload = await headers();
+    const svixHeaders = {
+        "svix-id": headerPayload.get("svix-id"),
+        "svix-signature": headerPayload.get("svix-signature"),
+    };
+
+    // get the payload and verify it
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+    const { data, type } = wh.verify(body, svixHeaders);
+
+    // prepare the user data to be saved in the database
+    const userData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+    };
+
+    await connectDB();
+}
